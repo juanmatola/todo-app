97af8b408d1c570eca44c6a2cde05f3b0ca44d01
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-11-16 20:33:47 -0300

    Implemento reaccion a mensajes de error

---
+++ src/main/resources/static/js/errorHandler.js
@@ -0,0 +1,4 @@
+import errorHandler from "./modules/errorHandlerFunciton.js";
+
+let url = new URL(document.URL);
+errorHandler(url);
---
+++ src/main/resources/static/js/modules/errorHandlerFunciton.js
@@ -0,0 +1,14 @@
+export default function errorHandler(url){
+	if (!(url.search === '')) {
+    
+	    let err = url.searchParams.get("err");
+	
+	    if (err != null) {
+	        let errorModalBody = document.getElementById("ErrorModalMesagge");
+	        let errModal = new bootstrap.Modal(document.getElementById("ErrorModal"));
+	        errorModalBody.innerHTML = "<p>"+ err + "</p>";
+	        errModal.show();
+	    }
+		
+	}
+}
